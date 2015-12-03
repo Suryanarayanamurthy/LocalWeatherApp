3501948da7426c60c8aaa96c34ae5450bc2e3839
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-12-03 15:48:22 +0100

    geo location
    getting the users geo location

--- src/weatherApp.js
+++ src/weatherApp.js
@@ -1,6 +1,19 @@
 
 var app = angular.module('wApp', []);
+var mysrclat= 0; var mysrclong = 0;
 
 app.controller('weatherController', function($scope){
     $scope.test = "Hello Ajs-1.5";
+
+  $scope.nearme = function($scope) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+                mysrclat = position.coords.latitude; 
+                mysrclong = position.coords.longitude;
+        });
+        console.log(mysrclat);
+        console.log(mysrclong);
+    }
+}
 });
