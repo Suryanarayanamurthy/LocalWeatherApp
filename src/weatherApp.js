8b2a5b0f7a27949cc7caad0904bb0a9020948bd5
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-12-09 02:05:15 +0100

    working version
    need todo lot of cleanup

--- src/weatherApp.js
+++ src/weatherApp.js
@@ -1,14 +1,23 @@
 
 var app = angular.module('wApp', []);
-var mysrclat = 0;
-var mysrclong = 0;
 
-app.controller('weatherController', function ($scope,$http) {
-    
+
+app.controller('weatherController', ['$scope', 'weatherService', function ($scope, weatherService) {
+    var mysrclat = 0;
+var mysrclong = 0;
     $scope.getLocalWeather = function(){
-        YahooWeatherAPI();
+         $scope.place = '';
+        getCurrentLocation();
+        if(mysrclat !== 0 && mysrclong !== 0)
+    fetchWeather(mysrclat,mysrclong);
     }
-  
+    
+  function fetchWeather(lat,long) {
+    weatherService.getWeather(lat,long).then(function(data){
+      $scope.place = data;
+    }); 
+  }
+    fetchWeather(52.4851659,13.4383258);
     function getCurrentLocation(){
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -80,4 +89,23 @@ var yql_base_uri = "https://query.yahooapis.com/v1/public/yql?q=";
     
     
     
-});
+}]);
+
+app.factory('weatherService', ['$http', '$q', function ($http, $q){
+  function getWeather (lat,long) {
+    var deferred = $q.defer();
+    $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.placefinder%20WHERE%20text%3D%22"+lat+"%2C"+long+"%22%20and%20gflags%3D%22R%22)&format=json&callback=")
+      .success(function(data){
+        deferred.resolve(data.query.results.channel);
+      })
+      .error(function(err){
+        console.log('Error retrieving markets');
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  }
+  
+  return {
+    getWeather: getWeather
+  };
+}]);
